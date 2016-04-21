d373891242dc07074c898f217677d228f5dc5c83
Geroshabu <oonstks@gmail.com>
2016-04-21 13:35:33 +0900

    refs #9 Return basal value if 'n' is negative
    https://github.com/Geroshabu/HaskellTraining/pull/11#issuecomment-209749920
    に対する修正.

--- Chapter1_Base/list1.hs
+++ Chapter1_Base/list1.hs
@@ -4,11 +4,11 @@ sum' (x:xs) = x + (sum' xs)
 product' [] = 1
 product' (x:xs) = x * (product' xs)
 
-take' 0 xs = []
+take' n xs | n < 1 = []
 take' n [] = []
 take' n (x:xs) = x : (take' (n-1) xs)
 
-drop' 0 xs = xs
+drop' n xs | n < 1 = xs
 drop' n [] = []
 drop' n (x:xs) = drop' (n-1) xs
 
