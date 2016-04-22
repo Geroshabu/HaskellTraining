a14766c5407269fdfc3f5ce397fcb6c0f8562500
Geroshabu <oonstks@gmail.com>
2016-04-22 12:46:41 +0900

    refs #9 Clarify unused parameters
    https://github.com/Geroshabu/HaskellTraining/pull/11#issuecomment-206369641 に対する修正.

--- Chapter1_Base/list1.hs
+++ Chapter1_Base/list1.hs
@@ -4,12 +4,12 @@ sum' (x:xs) = x + (sum' xs)
 product' [] = 1
 product' (x:xs) = x * (product' xs)
 
-take' n xs | n < 1 = []
-take' n [] = []
+take' n _ | n < 1 = []
+take' _ [] = []
 take' n (x:xs) = x : (take' (n-1) xs)
 
 drop' n xs | n < 1 = xs
-drop' n [] = []
+drop' _ [] = []
 drop' n (x:xs) = drop' (n-1) xs
 
 reverse' [] = []
